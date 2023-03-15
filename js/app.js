467b88c806c2d12cefb3a6a43a48ebc3474627c6
majdiShomali <majdi.shomali.1997@gmail.com>
2023-03-15 16:08:07 +0200

    edit js file

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-let name =prompt("what is your name ");
-let gender =prompt("what is your Gender male/female");
+let name =prompt("what is your name ? ");
+let gender =prompt("what is your Gender male/female ?");
 if(gender=="male"){
-alert(`welcome MR ${name} `);
+alert(`welcome MR ${name} !`);
 }else if(gender=="female"){
-alert(`welcome MS ${name} `);
+alert(`welcome MS ${name} !`);
 }
 else{
-    alert("welcome");
+    alert("welcome!");
 }
 
-let drinkType =prompt("do you want hot or cold drink");
-let drinkName =prompt("what is your drink name");
+let drinkType =prompt("do you want hot or cold drink?");
+let drinkName =prompt("what is your drink name?");
 alert("your drink is getting prepared ")
 console.log(` name:${name} Gender:${gender} drinkType:${drinkType}  drinkName:${drinkName}   ` );
