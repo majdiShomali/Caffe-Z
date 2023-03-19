a91869a1796428c63c7053b0b7bca6515d9f00d7
majdiShomali <majdi.shomali.1997@gmail.com>
2023-03-19 15:31:06 +0200

    js-task2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 let name =prompt("what is your name ? ");
 let gender =prompt("what is your Gender male/female ?");
 
-if(gender=="male"){
+
+while(gender!="male" &&  gender!="female"  ){
+gender =prompt("please enter you gender  male/female ?");
+}
+
+
+ if(gender=="male"){
 alert(`welcome MR ${name} !`);
 }else if(gender=="female"){
 alert(`welcome MS ${name} !`);
-}else{
-alert("welcome!");
 }
+
 
 let drinkType =prompt("do you want hot or cold drink?");
 let drinkName =prompt("what is your drink name?");
 alert("your drink is getting prepared ");
 
-console.log(`name:${name} Gender:${gender} drinkType:${drinkType}  drinkName:${drinkName}` );
+let orderInfo=[name, gender, drinkType ,drinkName];
+console.log(orderInfo );
